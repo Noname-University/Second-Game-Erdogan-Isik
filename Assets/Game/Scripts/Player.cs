2ac4f8b0cb1e4343740675b4d27e920a63eb5c6b
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-03-10 21:43:43 +0300

    Code refactoring.

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Helpers;
 
-public class Player : MonoBehaviour
+public class Player : MonoSingleton<Player>
 {
     [SerializeField]
     private float speed;
